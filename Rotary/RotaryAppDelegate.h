578baac42fcb3c23b97f9e46bf25ed92afc6953b
Evan Wu <wuzhiyong890926@gmail.com>
2013-07-09 20:05:03 +0800

    first commit

---
+++ Rotary/RotaryAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RotaryAppDelegate.h
+//  Rotary
+//
+//  Created by Evan on 13-7-9.
+//  Copyright (c) 2013年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotaryAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rotary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rotary
+//
+//  Created by Evan on 13-7-9.
+//  Copyright (c) 2013年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RotaryAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RotaryAppDelegate class]));
+    }
+}
